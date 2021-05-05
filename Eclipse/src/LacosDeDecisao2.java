659ff2478b5417da747db333c8c693b1919eedbf
rulevamanda <rulevamanda@gmail.com>
2021-05-05 13:35:35 -0300

    update exercises

--- Eclipse/src/LacosDeDecisao2.java
+++ Eclipse/src/LacosDeDecisao2.java
@@ -5,19 +5,21 @@ public class LacosDeDecisao2 {
 
 	public static void main(String[] args) {
 		//Faça um programa que entre com três números e coloque em ordem crescente.
+		int i,j;
+		
 		Scanner num = new Scanner(System.in);
 
 		int[] lista = new int[3];
-		for (int i = 0; i < lista.length; i++){
-			System.out.print("Digite o "+ (i+1) + "° número: "); 
+		for (i=0;i<lista.length;i++){
+			System.out.print("Digite o "+(i+1) + "° número: "); 
 			lista[i] = (num.nextInt());
 		}
 
 		Arrays.sort(lista);
 
 		System.out.print("Ordem crescente: "); 
-		for (int j = 0; j < lista.length; j++){
-			System.out.print(lista[j]+ " ");
+		for (j=0; j<lista.length;j++){
+			System.out.print(lista[j]+" ");
 		}
 	}
 }
