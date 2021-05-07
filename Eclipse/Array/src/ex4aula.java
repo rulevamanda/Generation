bf7bd335a2a7971d09242daef64f6e545ead7efa
rulevamanda <rulevamanda@gmail.com>
2021-05-07 16:54:48 -0300

    add new file 202105071654

---
+++ Eclipse/Array/src/ex4aula.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class ex4aula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float[][] matriz1 = new float[2][2];
+		float[][] matriz2 = new float[2][2];
+		float[][] matriz3 = new float[2][2];
+		int valor,l,c,op;
+		
+		Scanner leia = new Scanner(System.in);
+		for(l=0;l<2;l++) {
+			for(c=0;c<2;c++) {
+				System.out.println("\nMatriz1: ");
+				matriz1[l][c] = leia.nextFloat();
+				System.out.println("\nMatriz2: ");
+				matriz2[l][c] = leia.nextFloat();
+			}
+		}
+		System.out.println("\n\t\tMenu de opções...");
+		System.out.println("\n1- somar as duas matrizes");
+		System.out.println("\n2- subtrair aprimeira matriz da segunda");
+		System.out.println("\n3- adicionar uma constante às duas matrizes");
+		System.out.println("\n4- imprimir as matrizes");
+		System.out.println("\nDigite sua opção");
+		op = leia.nextInt();
+		
+		while(op<1||op>4) {
+			System.err.println("ERRO");
+			System.out.println("\nDigite uma opção válida");
+			System.out.println("\n\t\tMenu de opções...");
+			System.out.println("\n1- somar as duas matrizes");
+			System.out.println("\n2- subtrair aprimeira matriz da segunda");
+			System.out.println("\n3- adicionar uma constante às duas matrizes");
+			System.out.println("\n4- imprimir as matrizes");
+			System.out.println("\nDigite sua opção");
+			op = leia.nextInt();
+		}
+		switch(op){
+		case 1:
+			for(l=0;l<2;l++) {
+				for(c=0;c<2;c++) {
+					matriz3[l][c]=matriz1[l][c]+matriz2[l][c];
+					System.out.println("\nSoma matriz: "+matriz3[l][c]);
+				}
+			}
+			break;
+			
+		case 2:
+			for(l=0;l<2;l++) {
+				for(c=0;c<2;c++) {
+					matriz3[l][c]=matriz1[l][c]-matriz2[l][c];
+					System.out.println("\nDiferença matriz: "+matriz3[l][c]);
+				}
+			}
+			break;
+		
+		case 3:
+			
+			System.out.println("\nDigite um valor para a constante: ");
+			valor = leia.nextInt();
+			
+			for(l=0;l<2;l++) {
+				for(c=0;c<2;c++) {
+					matriz1[l][c]=matriz1[l][c]+valor;
+					matriz2[l][c]=matriz2[l][c]+valor;
+					System.out.println("\nMatriz1 somada à constante: "+matriz1[l][c]);
+					System.out.println("\nMatriz2 somada à constante: "+matriz2[l][c]);
+				}
+			}
+			break;
+		
+		case 4:
+			for(l=0;l<2;l++) {
+				for(c=0;c<2;c++) {
+					System.out.println("\nMatriz1 somada à constante: "+matriz1[l][c]);
+					System.out.println("\nMatriz2 somada à constante: "+matriz2[l][c]);
+				}
+			}
+			break;
+		
+		/*default:
+			System.out.println("\nOpção inválida!");
+		*/
+		
+		}
+	
+		
+		
+		
+		
+		
+
+	}
+
+}
--- Eclipse/Array/src/exercicio4_202105071305.java
+++ Eclipse/Array/src/exercicio4_202105071305.java
@@ -7,7 +7,7 @@ public class exercicio4_202105071305 {
 		Ofereça ao usuário um menu de opções:
 		(1) somar as duas matrizes
 		(2) subtrair a primeira matriz da segunda
-		(3) adicionar uma constante as duas matrizes
+		(3) adicionar uma constante às duas matrizes
 		(4) imprimir as matrizes
 		Nas duas primeiras opções uma terceira matriz 2 x 2 deve ser criada.
 		Na terceira opção o valor da constante deve ser lido
@@ -16,10 +16,128 @@ public class exercicio4_202105071305 {
 		
 		Scanner read = new Scanner(System.in);
 
-		float[][] m1 = new float[2][2];
-		float[][] m2 = new float[2][2];
+		float[][] matriz1 = new float[2][2];
+		float[][] matriz2 = new float[2][2];
+		float[][] matriz3 = new float[2][2];
+		
+		
+		int i,j,opcao;
+		float constante;
+		
+		//alimentando matriz1
+		System.out.println("\nAlimente a matriz 1: ");
+		for(i=0;i<2;i++) {
+			for(j=0;j<2;j++) {
+				System.out.print("["+(i+1)+"]["+(j+1)+"]:");
+				matriz1[i][j] = read.nextFloat();
+			}
+		}
+		
+		//alimentando matriz2
+		System.out.println("\nAlimente a matriz 2: ");
+		for(i=0;i<2;i++) {
+			for(j=0;j<2;j++) {
+				System.out.print("["+(i+1)+"]["+(j+1)+"]:");
+				matriz2[i][j] = read.nextFloat();
+			}
+		}
+		
+		System.out.print("\nEscolha uma opção:"
+				+ "\n1 somar as duas matrizes"
+				+ "\n2 subtrair a primeira matriz da segunda"
+				+ "\n3 adicionar uma constante às duas matrizes"
+				+ "\n4 imprimir as matrizes\n");
+		opcao = read.nextInt();
+		
+		while(opcao<1||opcao>4) {
+			System.err.println("\nOPÇÃO INVÁLIDA!");
+			System.out.print("\nEscolha uma opção:"
+					+ "\n1 somar as duas matrizes"
+					+ "\n2 subtrair a primeira matriz da segunda"
+					+ "\n3 adicionar uma constante às duas matrizes"
+					+ "\n4 imprimir as matrizes\n");
+			opcao = read.nextInt();
+		}
+		
+		switch(opcao){
+			case 1://somar as duas matrizes
+				
+				System.out.print("\nSoma das duas matrizes:\n");
 
+				for(i=0;i<2;i++) {
+					for(j=0;j<2;j++) {
+						matriz3[i][j] = matriz1[i][j]+matriz2[i][j];
+					}
+				}
+				for(i=0;i<2;i++) {
+					for(j=0;j<2;j++) {
+						System.out.print("["+matriz3[i][j]+"]");
+				    }  
+				    System.out.println(" ");
+				}
+				break;
+				
+			case 2://subtrair a primeira matriz da segunda
 
+				System.out.print("\nSubtraçã da primeira matriz da segunda matriz:\n");
+				
+				for(i=0;i<2;i++) {
+					for(j=0;j<2;j++) {
+						matriz3[i][j] = matriz2[i][j]-matriz1[i][j];
+					}
+					for(i=0;i<2;i++) {
+						for(j=0;j<2;j++) {
+							System.out.print("["+matriz3[i][j]+"]");
+					    }  
+					    System.out.println(" ");
+					}
+				}
+				break;
+			
+			case 3://adicionar uma constante às duas matrizes
+				System.out.print("\nDigite uma constante: ");
+				constante = read.nextFloat();
+				
+				System.out.print("\nMatriz 1 + constante:\n");
+				for(i=0;i<2;i++) {
+					for(j=0;j<2;j++) {
+						System.out.print("["+(matriz1[i][j]+constante)+"]");
+				    }  
+				    System.out.println(" ");
+				}
+				
+				System.out.print("\nMatriz 2 + constante:\n");
+				for(i=0;i<2;i++) {
+					for(j=0;j<2;j++) {
+						System.out.print("["+(matriz2[i][j]+constante)+"]");
+				    }  
+				    System.out.println(" ");
+				}
+				break;
+			
+			case 4://imprimir as matrizes
+				
+				System.out.print("\nA matriz 1 é:\n");
+
+				for(i=0;i<2;i++) {
+					for(j=0;j<2;j++) {
+						System.out.print("["+matriz1[i][j]+"]");
+				    }  
+				    System.out.println(" ");
+				}
+				
+				System.out.print("\nA matriz 2 é:\n");
+
+				for(i=0;i<2;i++) {
+					for(j=0;j<2;j++) {
+						System.out.print("["+matriz2[i][j]+"]");
+				    }  
+				    System.out.println(" ");
+				}
+				break;
+	
+		}	
 	}
-
 }
+
+		
