0910b8186bb9f1f490fb4833301b8df77155ab1f
rulevamanda <rulevamanda@gmail.com>
2021-05-20 18:46:16 -0300

    add new file 202105211846

--- MySQL/BancoDeDadosRelacional/exercico1_202105201643.sql
+++ MySQL/BancoDeDadosRelacional/exercico1_202105201643.sql
@@ -3,6 +3,7 @@ USE db_empresa;
 CREATE TABLE tb_funcionaries  (
     id_usuario INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255),
+    sobrenome VARCHAR(255),
     nascimento DATE,
     genero VARCHAR(255),
     salario FLOAT NULL,
@@ -10,13 +11,13 @@ CREATE TABLE tb_funcionaries  (
     PRIMARY KEY (id_usuario)
 );
 
-INSERT INTO tb_funcionaries (nome, nascimento, genero, salario)
+INSERT INTO tb_funcionaries (nome, sobrenome, nascimento, genero, salario)
 VALUES
-('Camila', '1981-03-12', 'fem', 2300.6),
-('Adalberto', '1981-06-03', 'masc', 2500.3),
-('Wellington', '1981-10-01', 'masc', 1500.0),
-('Alex', '1981-12-04', 'masc', 3200.3),
-('Giovanna', '1981-11-21', 'fem', 2200.7);
+('Camila', 'Albuquerque', '1981-03-12', 'fem', 2300.6),
+('Adalberto', 'Geniba', '1981-06-03', 'masc', 2500.3),
+('Wellington', 'Souza', '1981-10-01', 'masc', 1500.0),
+('Alex', 'Ramirez', '1981-12-04', 'masc', 3200.3),
+('Giovanna', 'Santos', '1981-11-21', 'fem', 2200.7);
 
 SELECT nome, salario
 FROM tb_funcionaries
@@ -27,5 +28,6 @@ FROM tb_funcionaries
 WHERE salario < 2000; 
 
 SET SQL_SAFE_UPDATES = 0;
+
 UPDATE tb_funcionaries
 SET salario = salario + 500;
---
+++ MySQL/BancoDeDadosRelacional/exercico2_202105201841.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE db_e_commerce;
+USE db_e_commerce;
+CREATE TABLE tb_produtos  (
+    id_usuario INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255),
+    tamanho VARCHAR(255),
+    cor VARCHAR(255),
+    embalagem VARCHAR(255),
+    valor FLOAT NULL,
+    UNIQUE (id_usuario),
+    PRIMARY KEY (id_usuario)
+);
+
+INSERT INTO tb_produtos (nome, tamanho, cor, embalagem, valor)
+VALUES
+('miniatura', 'pequeno', 'azul', 'caixa', 300.0),
+('caneca', 'médio', 'preto', 'caixa', 508.0),
+('camiseta', 'grande', 'roxo', 'envelope plástico', 880.0),
+('meias', 'pequeno', 'rosa', 'nenhuma', 380.0),
+('camiseta', 'pequeno', 'azul', 'envelope plástico', 808.0),
+('cinto', 'médio', 'rosa', 'caixa', 370.0),
+('caderno', 'médio', 'branco', 'nenhuma', 350.0);
+
+
+SELECT nome, valor
+FROM tb_produtos
+WHERE valor > 500; 
+
+SELECT nome, valor
+FROM tb_produtos
+WHERE valor < 500; 
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE tb_produtos
+SET valor = valor - valor*0.15
+WHERE nome = 'caneca';
