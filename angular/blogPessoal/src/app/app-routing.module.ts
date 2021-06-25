a6c03ae7d0e880785d9072d27129ad10a60cacd1
rulevamanda <rulevamanda@gmail.com>
2021-06-25 11:23:01 -0300

    add route files 202106251122

--- angular/blogPessoal/src/app/app-routing.module.ts
+++ angular/blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { EntrarComponent } from "./entrar/entrar.component";
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from "./cadastrar/cadastrar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar',component: CadastrarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
