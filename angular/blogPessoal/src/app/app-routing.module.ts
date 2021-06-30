0d7100a0421d62c0686b12204f23a70b6a1f51f1
rulevamanda <rulevamanda@gmail.com>
2021-06-29 22:51:43 -0300

    add security 202106292251

--- angular/blogPessoal/src/app/app-routing.module.ts
+++ angular/blogPessoal/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { EntrarComponent } from "./entrar/entrar.component";
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from "./cadastrar/cadastrar.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
 const routes: Routes = [
 
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
   {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar',component: CadastrarComponent}
+  {path: 'cadastrar',component: CadastrarComponent},
+
+  {path: 'inicio', component: InicioComponent}
 
 ];
 
