9a9fee517413772e9b564b1f5fa5485f7051604a
rulevamanda <82651544+rulevamanda@users.noreply.github.com>
2021-07-13 15:28:02 -0300

    Delete angular/blogPessoal directory

--- angular/blogPessoal/src/app/model/UserLogin.ts
+++
@@ -1,9 +0,0 @@
-export class UserLogin{
-    public idUsuario: number    //id
-    public nome: string
-    public email: string        //usuario 
-    public senha: string
-    public token: string
-    public foto: string
-    public tipo: string
-}
